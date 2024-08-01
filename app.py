703744b081d07f40739252e55be7d750cb6bee83
Lavender <lhwang914@gmail.com>
2024-08-01 11:51:54 -0700

    Setup SQLite3 Databases

--- app.py
+++ app.py
@@ -3,41 +3,135 @@ from flask import Flask, flash, redirect, render_template, request, session
 import requests
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 app = Flask(__name__)
-
-#setting up databases
-app.config['SECRET_KEY'] = 'j8s$3g#7lB8%yWz!2Qk@eN9xCp4+F1uR'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app)
 
 #Using Spoonacular API
 API_KEY = '8e8ffa3209a745789ab98452dd1a8791'
 BASE_URL = 'https://api.spoonacular.com/recipes'
 
+app.config['SECRET_KEY'] = 'j8s$3g#7lB8%yWz!2Qk@eN9xCp4+F1uR'
+
+#setting up databases
+def init_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    # Setting up basic schema 
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE NOT NULL,
+                password TEXT UNIQUE NOT NULL)
+                """)
+    cursor.execute("""CREATE TABLE IF NOT EXISTS saved (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                recipe_name TEXT,
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users (id)
+                )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS cart (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                recipe_name TEXT,
+                user_id INTEGER,
+                FOREIGN KEY (user_id) REFERENCES users (id)
+                )""")
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+# Database utility functions
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    return conn
+
 
 @app.route('/')
 def home():
+    """Home page"""
     return render_template('layout.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    """Registers new users"""
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        confirmation = request.form.get("confirmation")
+
+        # Check for nonempty input
+        if not username or not password:
+            return "Username and password required", 400
+        
+        if confirmation != password:
+            return "Passwords must match", 400
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        # Ensure username does not already exist
+        try:
+            cursor.execute("INSERT INTO users(username, password) VALUES (?, ?)", ( username, generate_password_hash(password)))
+            conn.commit()
+        except sqlite3.DatabaseError:
+            return "username already exists", 400
+        finally: 
+            cursor.close()
+            conn.close()
+
+        login()
+        return redirect("/")
+
     return render_template('register.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('progress.html')
+    """Logs in existing users"""
+
+    #Forgets user_id
+    session.clear()
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        #check for nonempty input
+        if not username or not password:
+            return "Username and password required", 400
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        #query database for username
+        user = cursor.execute(
+            "SELECT * FROM users WHERE username = ?", (username)
+        ).fetchone()
+        cursor.close()
+        conn.close()
+
+        # Ensure username exists and password is correct
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            return redirect("/")
+        else:
+            return "Invalid username or password", 400
+
+    return render_template("login.html")
+
 
 @app.route('/logout')
 def logout():
-    return render_template('progress.html')
+    """Logs out existing users"""
+    session.clear()
+
+    return redirect("/")
 
 
 
 @app.route('/search', methods=['GET','POST'])
 def search():
+    """Search for recipes based on ingredients"""
     if request.method == "POST":
         ingredients = request.form.get('ingredients', '')
         numOfRecipes = request.form.get('numOfRecipes', '')
@@ -54,7 +148,7 @@ def search():
         data = response.json()
         recipes = data
 
-        return render_template('recipes.html', recipes=recipes, missing_ingredients=missing_ingredients)
+        return render_template('recipes.html', recipes=recipes)
     else: 
         
         return render_template('search.html')
@@ -62,14 +156,18 @@ def search():
                  
 @app.route('/saved')
 def saved():
+    """Access saved recipes"""
 
     return render_template('progress.html')
 
 
 @app.route('/cart')
 def cart():
+    """Adds recipes to a cart to generate a grocery list of missing ingredients"""
     return render_template('progress.html')
 
-
 if __name__ == '__main__':
+    init_db()
     app.run(debug=True)
+
+  
